171a3b02c1d5a9e6ef46dde6cfd83454e6a80efe
fergodani <ferreiragomezdaniel@gmail.com>
2023-09-27 20:30:03 +0200

    Avanzado ejercicio 1 xml

--- XML/Ejercicio_1/js/main.js
+++ XML/Ejercicio_1/js/main.js
@@ -1,121 +1,92 @@
-// Contenido de ejemplo de un archivo OPF en formato de cadena
-const opfContent = `
-<package xmlns="http://openebook.org/namespaces/oeb-package/1.0/">
-  <metadata>
-    <dc:title>Título del Libro</dc:title>
-    <dc:creator>Nombre del Autor</dc:creator>
-    <dc:language>es</dc:language>
-  </metadata>
-  <manifest>
-    <item id="cover" href="cover.html" media-type="text/html" />
-    <item id="chapter1" href="chapter1.html" media-type="text/html" />
-  </manifest>
-  <spine>
-    <itemref idref="cover" />
-    <itemref idref="chapter1" />
-  </spine>
-</package>
-`;
+"use strict";
+class XMLFile {
+  constructor() {
 
-// Crear un parser DOM para analizar el archivo OPF
-const parser = new DOMParser();
-const xmlDoc = parser.parseFromString(opfContent, 'application/xml');
+  }
 
-// Extraer metadatos del libro
-const title = xmlDoc.querySelector('dc\\:title').textContent;
-const author = xmlDoc.querySelector('dc\\:creator').textContent;
-const language = xmlDoc.querySelector('dc\\:language').textContent;
+  createElement(type, text) {
+    var element = document.createElement(type);
+    element.innerHTML = text;
+    $("h5").text(text);
+  }
 
-console.log('Título del Libro:', title);
-console.log('Autor:', author);
-console.log('Idioma:', language);
+  verXML(text) {
+    this.createElement("p", text)
+  }
 
-// Extraer información del manifiesto (lista de recursos)
-const items = xmlDoc.querySelectorAll('manifest item');
-console.log('Recursos en el Manifiesto:');
-items.forEach(item => {
-  const id = item.getAttribute('id');
-  const href = item.getAttribute('href');
-  const mediaType = item.getAttribute('media-type');
-  console.log(`- ID: ${id}, HREF: ${href}, Tipo de Medio: ${mediaType}`);
-});
+  readLibrary() {
+    $.ajax({
+      dataType: "xml",
+      url: "./ebooks/book1/book1.xml",
+      method: 'GET',
+      success: function(datos) {
+        const title = datos.firstElementChild.firstElementChild.firstElementChild.children[0].innerHTML
+        const type = datos.firstElementChild.firstElementChild.firstElementChild.children[1].innerHTML
+        const author = datos.firstElementChild.firstElementChild.firstElementChild.children[3].innerHTML
+        const cover = "./ebooks/book1/" + datos.firstElementChild.children[1].children[0].attributes.getNamedItem("href").value
+        $("main").html(
+          "<article><img src='"+cover+"' /><h2>" + title + "</h2><p>" + author +"</p></article>"
+        )
+        $("article").after(
+          "<article><img src='"+cover+"' /><h2>" + title + "</h2><p>" + author +"</p></article>"
+        )
+      }
+    })
+  
+  }
 
-// Extraer información de la columna vertebral (orden de lectura)
-const itemrefs = xmlDoc.querySelectorAll('spine itemref');
-console.log('Orden de Lectura (Columna Vertebral):');
-itemrefs.forEach(itemref => {
-  const idref = itemref.getAttribute('idref');
-  console.log(`- ID de Referencia: ${idref}`);
-});
-
-// Obtener el contenedor HTML donde mostraremos la información del libro
-const bookInfoContainer = document.getElementById('book-info');
-
-// Crear elementos HTML para mostrar la información
-const titleElement = document.createElement('h2');
-titleElement.textContent = 'Título del Libro: ' + title;
-
-const authorElement = document.createElement('p');
-authorElement.textContent = 'Autor: ' + author;
-
-const languageElement = document.createElement('p');
-languageElement.textContent = 'Idioma: ' + language;
-
-// Agregar los elementos al contenedor
-bookInfoContainer.appendChild(titleElement);
-bookInfoContainer.appendChild(authorElement);
-bookInfoContainer.appendChild(languageElement);
-
-
-// Obtener el contenedor HTML donde mostraremos la lista de libros
-const bookListContainer = document.getElementById('book-list');
-
-// Lista de nombres de archivos OPF (representando los libros)
-const bookFiles = ['libro1.opf', 'libro2.opf', 'libro3.opf']; // Reemplaza con tus nombres de archivos reales
-
-// Función para agregar un elemento de libro a la lista
-function addBookToList(title, author) {
-    const listItem = document.createElement('li');
-    listItem.innerHTML = `<strong>${title}</strong> - ${author}`;
-    bookListContainer.appendChild(listItem);
+  leerArchivoTexto(files) 
+  { 
+      //Solamente toma un archivo
+      //var archivo = document.getElementById("archivoTexto").files[0];
+      var archivo = files[0];
+      var contenido = document.getElementById("contenidoArchivo");
+      var areaVisualizacion = document.getElementById("areaTexto");
+      var errorArchivo = document.getElementById("errorLectura");
+    
+      contenido.innerText="Contenido del archivo de texto:"
+      //Solamente admite archivos de tipo texto
+      var tipoTexto = /text.*/;
+      if (archivo.type.match(tipoTexto)) 
+        {
+          var lector = new FileReader();
+          lector.onload = function (evento) {
+            //El evento "onload" se lleva a cabo cada vez que se completa con éxito una operación de lectura
+            //La propiedad "result" es donde se almacena el contenido del archivo
+            //Esta propiedad solamente es válida cuando se termina la operación de lectura
+            //areaVisualizacion.innerText = lector.result;
+            const xml = new XMLFile()
+            console.log(lector.result)
+            const parser = new DOMParser();
+            const xmlDoc = parser.parseFromString(lector.result, 'application/xml');
+            var str = (new XMLSerializer()).serializeToString(xmlDoc)
+            xml.verXML(str)
+            console.log(str)
+            console.log("Total nodos: " + $('*', lector.result).length)
+            $.ajax({
+              dataType: "xml",
+              url: "./ebooks/book1/book1.xml",
+              method: 'GET',
+              success: function(datos) {
+                console.log(datos)
+                console.log("Total nodos: " + $('*', datos).length)
+                console.log("Título: " + $('dc:Title', datos))
+                console.log("Item: " + $('item', datos).attr("cover"))
+              }
+            })
+            }      
+          lector.readAsText(archivo);
+          }
+      else {
+          errorArchivo.innerText = "Error : ¡¡¡ Archivo no válido !!!";
+          }       
+  };
 }
 
-// Iterar sobre los archivos OPF y mostrar la lista de libros
-bookFiles.forEach(bookFile => {
-    // Crear un parser DOM para analizar el archivo OPF
-  const parser = new DOMParser();
-  const xmlDoc = parser.parseFromString(opfContent, 'application/xml');
+let xml = new XMLFile()
+xml.readLibrary()
 
-  // Extraer metadatos del libro
-  const title = xmlDoc.querySelector('dc\\:title').textContent;
-  const author = xmlDoc.querySelector('dc\\:creator').textContent;
-
-    // Agregar el libro a la lista
-    addBookToList(title, author);
-});
-
-const fileInput = document.getElementById('file-input');
-        const fileContents = document.getElementById('file-contents');
-
-        fileInput.addEventListener('change', function() {
-            const selectedFile = fileInput.files[0];
-
-            if (selectedFile) {
-                const reader = new FileReader();
-
-                reader.onload = function(event) {
-                    const fileText = event.target.result;
-                    fileContents.textContent = fileText;
-                };
-
-                reader.readAsText(selectedFile);
-            } else {
-                fileContents.textContent = 'Ningún archivo seleccionado';
-            }
-        });
-
-
-
+ 
 
 
 
